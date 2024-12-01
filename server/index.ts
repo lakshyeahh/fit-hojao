bfddfe1dd449f3157174f6f18a321351ebf64f66
lakshyeahh <verma.lakshya071@gmail.com>
2024-12-01 14:12:11 +0530

    server

--- server/index.ts
+++ server/index.ts
@@ -22,9 +22,18 @@ const asyncHandler = (fn: Function) => (req: Request, res: Response) => {
 
 // Configure CORS
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://guardian-main.vercel.app'] 
-    : 'http://localhost:5173',
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'https://guardian-main.vercel.app',
+      'http://localhost:5173',
+      'http://localhost:3000'
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
